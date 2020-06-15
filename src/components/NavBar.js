dc342e74fa36ae9a514edbc2367448a9ee0463de
Vivica <Ma>
2020-06-15 15:19:07 -0400

    our first route

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <p>Login</p>
+        <p>Log out</p>
+        <p>All Hikes</p>
+        <p>Your Hike</p>
+      
+      </div>
+    )
+  }
+}
+
+export default NavBar
+
+
+
+
---
+++ src/components/WelcomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class WelcomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome To Hiking Club</h1>
+      </div>
+    )
+  }
+}
+
+export default WelcomePage
