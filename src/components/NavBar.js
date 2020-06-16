332ddb06860e42ae8bace59e20bb0dd61dbfe57b
Vivica <Ma>
2020-06-15 21:53:30 -0400

    styling the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,22 +3,30 @@ import { Link, NavLink } from 'react-router-dom';
 
 
 export class NavBar extends Component {
-  // state ={
-  //   on: false
-  // }
-
-  // toggle = () =>{
-  //   this.setState({on: !this.state.on})
-  // }
+  
   render() {
-  return(
-    <div className="main-menu">
-      <Link to="/"> <div>logo</div> </Link>
-      <Link to="/hikes"><div>Hikes</div></Link>
-      <Link to="/members"><div>Members</div></Link>
-      <Link to="/"><div>Log out</div></Link> 
-      </div>
 
+  const link = {
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    background: 'black',
+    textDecoration: 'none',
+    color: 'white',
+  }
+  return(
+    // <div className="main-menu">
+    //   <Link to="/"> <div>logo</div> </Link>
+    //   <Link to="/hikes"><div>Hikes</div></Link>
+    //   <Link to="/members"><div>Members</div></Link>
+    //   <Link to="/"><div>Log out</div></Link> 
+    //   </div>
+            <div className="main-menu">
+            <NavLink to="/" exact style={link} activeStyle={{background: "blue"}}>🦚</NavLink>
+            <NavLink to="/hikes" exact style={link} activeStyle={{background: "blue"}}>Hikes</NavLink>
+            <NavLink to="/members" exact style={link} activeStyle={{background: "blue"}}>Members</NavLink>
+            <NavLink to="/" exact style={link} activeStyle={{background: "green"}}>Log out</NavLink>
+            </div>
     )
   }
 }
