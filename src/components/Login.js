2834780a66d4adbb4f8bc83baffd5788ccfe5c8c
Vivica <Ma>
2020-06-15 22:56:42 -0400

    signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
  
 class Login extends Component {
+
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleChange = e = this.setState({ [e.target.name]: e.target.value})
   render() {
+    const {username, password} =this.state
+    console.log("login🌳🦚🐓", this.state)
     return (
       <form>
         <h1>Login</h1>
         <div>
-          <input type="text" name="username" placeholder="Username" />
-          <label htmlFor="username">Username</label>
+          <label htmlFor="username">Username: </label>
+          <input type="text" name="username" placeholder="Username" value={username} onChange={this.handleChange}/>
         </div>
+        <br></br>
         <div>
-          <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password" placeholder="Password"  value={password} onChange={this.handleChange}/>
         </div>
+        <br></br>
         <input type="submit" value="Login" />
       </form>
     );
