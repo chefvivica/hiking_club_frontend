fd2ee7b85590261f741c304f45b5aff5162adde9
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-06-15 20:56:37 -0700

    fixed handlesubmit in login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,21 +7,36 @@ class Login extends Component {
     password: ''
   }
 
-  handleChange = e = this.setState({ [e.target.name]: e.target.value})
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value})
+  }
+
   render() {
-    const {username, password} =this.state
+    const {username, password} = this.state
     console.log("login🌳🦚🐓", this.state)
     return (
       <form>
         <h1>Login</h1>
         <div>
           <label htmlFor="username">Username: </label>
-          <input type="text" name="username" placeholder="Username" value={username} onChange={this.handleChange}/>
+          <input 
+            type="text" 
+            name="username" 
+            placeholder="Username" 
+            value={username} 
+            onChange={this.handleChange}
+          />
         </div>
         <br></br>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" name="password" placeholder="Password"  value={password} onChange={this.handleChange}/>
+          <input 
+            type="password" 
+            name="password" 
+            placeholder="Password" 
+            value={password} 
+            onChange={this.handleChange}
+          />
         </div>
         <br></br>
         <input type="submit" value="Login" />
