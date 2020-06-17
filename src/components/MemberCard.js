9d9025e940542051e9b6f2ce82ac6ef371b31f13
Vivica <Ma>
2020-06-17 12:23:53 -0400

    added basic styling for members

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -5,10 +5,10 @@ const MemberCard = (props) => {
   const { name, username, img_url } = props
 
   return (
-    <div>
+    <div className="members">
       <img src={img_url} alt={name}></img>
       <h3>{name}</h3>
-      <h5>{username}</h5>
+      <h5>user name: {username}</h5>
     </div>
   )
 
